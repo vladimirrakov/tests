d51714ae8a078e5eb7e8549bdbb0694bbb13e12c
Vladimir Rakov <128072347+vladimirrakov@users.noreply.github.com>
2023-12-12 12:36:20 +0100

    Draft new code

--- tests/bookerPageEmailTestFromJson.spec.ts
+++ tests/bookerPageEmailTestFromJson.spec.ts
@@ -1,31 +1,21 @@
 import { test, expect } from "@playwright/test";
 
-import fs from "fs";
-import path from "path";
-// import { test, expect, type Page } from '@playwright/test';
+const emails = ["name@email.com", "nameemail.com", "name@@email.com"];
 
-const data = fs.readFileSync(path.join(__dirname, "booking-test.json"), "utf8");
-let testData = JSON.parse(data.toString());
-
-// TESTS HERE
-test.beforeEach(async ({ page }) => {
-    //1. (ARRANGE) GOTO Restful Booker main page
+for (const email of emails) {
+  test(`testing with ${email}`, async ({ page }) => {
+    //1. (ARRANGE) Go to Restful Booker main page
     await page.goto("https://booker.govza.com/");
-});
 
-// A CYCLE BEGINS HERE FOR testData
-
-testData.forEach((element) => {
-  test("email input value", async ({ page }) => {
-    // 2. (ACT) Make a booking with some invalid fields
+    // 2. (ACT) Make a booking with test data
     await page.getByRole("button", { name: "Book this room" }).click();
-    await page.locator('input[name="phone"]').click();
-    await page.locator('input[name="phone"]').fill("123");
+    await page.locator('input[name="email"]').click();
+    await page.locator('input[name="email"]').fill(email);
     await page.getByRole("button", { name: "Book" }).click();
-    
+
     // 3. (ASSERT) Assert that the error is showed
     await expect(page.locator("#root")).toContainText(
-      "size must be between 11 and 21"
+      "must be a well-formed email address"
     );
   });
-});
+}
